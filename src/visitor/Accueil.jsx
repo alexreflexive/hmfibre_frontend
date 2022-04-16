c065c6d40daf6982a68620d3a5730780c26e2d71
Alejandro GABRIEL Y GALAN <alexgyg@msn.com>
2022-04-16 14:29:11 +0200

    intermediaire

--- src/visitor/Accueil.jsx
+++ src/visitor/Accueil.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 import "./Accueil.css";
 
 function Accueil() {
+  const slideImages = [
+    "../../public/images/slides/slide_1.jpg",
+    "../../public/images/slides/slide_2.jpg",
+    "../../public/images/slides/slide_3.jpg",
+  ];
   return (
     <div id="accueil">
-      <div id="accueilDroite" className="accueil-panneaux">
+      <div className="slide-container">
+        <Slide>
+          {slideImages.map((slideImage, index) => (
+            <div className="each-slide" key={index}>
+              <img src={slideImage} alt="" width="100%" />
+            </div>
+          ))}
+        </Slide>
+      </div>
+      {/* <div id="accueilDroite" className="accueil-panneaux">
         <div className="panneau-texte">
           <h4>Un service de qualité</h4>
           <p>
@@ -13,9 +29,11 @@ function Accueil() {
             entreprises pour les accompagner dans leur projet.
           </p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
 
 export default Accueil;
+
+//  background-image: url("../../public/images/photo-3.jpg");
